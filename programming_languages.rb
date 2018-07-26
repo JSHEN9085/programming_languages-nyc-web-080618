ec0b7a48befc80c4320e210d5f0770d6e8cbcad8
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-26 12:50:59 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,24 +1,3 @@
-# def reformat_languages(languages)
-#   # your code here
-# end
-
-# def reformat_languages(languages)
-# new_hash = Hash.new
-#   hash.each do |type, language|
-#   language.each do |name, info|
-#     new_hash[name] = info
-#   if hash[:oo].has_key?(name)
-#     new_hash[name][:style] = []
-#     new_hash[name][:style].push(:oo)
-#     end
-#   if hash[:interpreted].has_key?(name)
-#     new_hash[name][:style].push(:interpreted)
-#     end
-# end
-# end
-# puts new_hash
-# end
-
  def reformat_languages(languages)
   # your code here
   new_hash = {}
